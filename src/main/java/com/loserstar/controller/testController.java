a4b66f0ab69da822921c4d5fc2a109eed90d6163
loserStar <362527240@qq.com>
2017-03-08 13:01:06 +0800

    初始化sqlSessionFactory时候还有点问题

--- src/main/java/com/loserstar/controller/testController.java
+++ src/main/java/com/loserstar/controller/testController.java
@@ -1,11 +1,17 @@
 package com.loserstar.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.loserstar.dao.UserDao;
+import com.loserstar.entity.User;
 
 @Controller
 @RequestMapping(value="/test")
@@ -20,14 +26,20 @@ public class testController{
 	
 	@RequestMapping(value="json")
 	@ResponseBody
-	public Map<String, String> json(){
-		Map<String, String> returnMap = new HashMap<String,String>();
-		returnMap.put("1", "json");
-		returnMap.put("2", "html");
-		returnMap.put("3", "javacript");
-		returnMap.put("code", "1");
+	public Map<String, Object> json(){
+		Map<String, Object> returnMap = new HashMap<String,Object>();
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+		UserDao userDao = (UserDao)applicationContext.getBean("userDao");
+		User userParam = new User();
+		userParam.setUserName("b");
+		List<User> userList = userDao.findList(userParam);
+		
+		returnMap.put("code", 1);
+		returnMap.put("data", userList);
 		
 		return returnMap;
 	}
+	
+	
 
 }
